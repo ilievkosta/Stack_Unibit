f7c8ca3ba460d80eb0272387564009094b29efdf
ilievkosta <47809260+ilievkosta@users.noreply.github.com>
2021-03-21 16:35:45 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Stack_Unibit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.richTextBoxEx = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnExample = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxAnswer);
+            this.panel1.Controls.Add(this.richTextBoxEx);
+            this.panel1.Location = new System.Drawing.Point(27, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 303);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Отговор";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Въведете израза";
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Location = new System.Drawing.Point(18, 241);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(349, 20);
+            this.textBoxAnswer.TabIndex = 1;
+            // 
+            // richTextBoxEx
+            // 
+            this.richTextBoxEx.Location = new System.Drawing.Point(18, 42);
+            this.richTextBoxEx.Name = "richTextBoxEx";
+            this.richTextBoxEx.Size = new System.Drawing.Size(353, 141);
+            this.richTextBoxEx.TabIndex = 0;
+            this.richTextBoxEx.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(457, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Провери израза";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "()",
+            "{}",
+            "(),{}"});
+            this.listBox1.Location = new System.Drawing.Point(457, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(75, 43);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(457, 151);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 3;
+            this.btnHelp.Text = "Помощ";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnExample
+            // 
+            this.btnExample.Location = new System.Drawing.Point(457, 122);
+            this.btnExample.Name = "btnExample";
+            this.btnExample.Size = new System.Drawing.Size(75, 23);
+            this.btnExample.TabIndex = 4;
+            this.btnExample.Text = "Пример";
+            this.btnExample.UseVisualStyleBackColor = true;
+            this.btnExample.Click += new System.EventHandler(this.btnExample_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 371);
+            this.Controls.Add(this.btnExample);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(580, 410);
+            this.Name = "Form1";
+            this.Text = "Проверка за правилен брой затварящи скоби";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.RichTextBox richTextBoxEx;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnExample;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace Stack_Unibit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            string selectedItem = listBox1.Items[listBox1.SelectedIndex].ToString();
+            switch (selectedItem)
+            {
+                case "()":
+
+
+                    if (FirstBrackets(richTextBoxEx.Text))
+                    {
+                        textBoxAnswer.Text = "Израза е верен";
+                    }
+                    else
+                    {
+                        textBoxAnswer.Text = "Израза е грешен";
+                    }
+                    break;
+
+                case "{}":
+
+
+                    if (SecondBrackets(richTextBoxEx.Text))
+                    {
+                        textBoxAnswer.Text = "Израза е верен";
+                    }
+                    else
+                    {
+                        textBoxAnswer.Text = "Израза е грешен";
+                    }
+                    break;
+
+                case "(),{}":
+
+
+                    if (FirstBrackets(richTextBoxEx.Text) & SecondBrackets(richTextBoxEx.Text))
+                    {
+                        textBoxAnswer.Text = "Израза е верен";
+                    }
+                    else
+                    {
+                        textBoxAnswer.Text = "Израза е грешен";
+                    }
+                    break;
+            }
+        }
+
+        private Boolean FirstBrackets(string expr)
+        {
+
+            string expression = expr;
+
+
+            Stack<int> stack = new Stack<int>();
+
+            bool correctBrackets = true;
+
+
+
+            for (int index = 0; index < expression.Length; index++)
+
+            {
+
+                char ch = expression[index];
+
+                if (ch == '(')
+
+                {
+
+                    stack.Push(index);
+
+                }
+
+                else if (ch == ')')
+
+                {
+
+                    if (stack.Count == 0)
+
+                    {
+
+                        correctBrackets = false;
+
+                        break;
+
+                    }
+
+                    stack.Pop();
+
+                }
+
+            }
+
+            if (stack.Count != 0)
+
+            {
+
+                correctBrackets = false;
+
+            }
+            if (correctBrackets)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private Boolean SecondBrackets(string expr)
+        {
+
+            string expression = expr;
+
+
+            Stack<int> stack = new Stack<int>();
+
+            bool correctBrackets = true;
+
+
+
+            for (int index = 0; index < expression.Length; index++)
+
+            {
+
+                char ch = expression[index];
+
+                if (ch == '{')
+
+                {
+
+                    stack.Push(index);
+
+                }
+
+                else if (ch == '}')
+
+                {
+
+                    if (stack.Count == 0)
+
+                    {
+
+                        correctBrackets = false;
+
+                        break;
+
+                    }
+
+                    stack.Pop();
+
+                }
+
+            }
+
+            if (stack.Count != 0)
+
+            {
+
+                correctBrackets = false;
+
+            }
+            if (correctBrackets)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Помощ при използване на програмата";
+            popup.ContentText = "Моля въведете израза и след това изберете" +
+                "за кой тип скоби искате да проверите" +
+                " Избор едно - (),Избор две - {}, Избор три -" +
+                " и за двата вида скоби едновременно." +
+                " Натиснете бутона Провери";
+            popup.Popup();
+        }
+
+        private void btnExample_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Примерен израз";
+            popup.ContentText = "{((4+5)+(7*4))/(2+3)}";
+            popup.Popup();
+        }
+    }
+}
